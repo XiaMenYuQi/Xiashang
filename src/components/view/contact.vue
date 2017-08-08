3661fcae66987b409a427f1da9377367b0628ef7
cry101 <919907366@qq.com>
2017-08-08 14:56:46 +0800

    4

--- src/components/view/contact.vue
+++ src/components/view/contact.vue
@@ -6,10 +6,9 @@
         <div class="resp-tabs-container hor_1">
           <div>
             <div>
-              <p style="font-weight:bold;color:#ff002b;margin-bottom:15px;">厦门销售中心</p>
-              地址：厦门市湖里区护安路73-75号湖里高新技术园西区梅花大厦一楼Google AdWords体验中心<br />
+              地址：云南省昆明市建设路296号银燕中心11层<br />
               <br />
-              电话：0592-5553237&nbsp;<br />
+              电话：13132139813&nbsp;<br />
               <br />
               邮箱：4501897@qq.com                    </div>
           </div>
@@ -38,5 +37,5 @@
 </script>
 
 <style scope>
-  .resp-tabs-container{height: 310px;}
+  .resp-tabs-container{height: 340px;}
 </style>
